151f02b752dcc54cb9b2c1563df04c1005ff24ee
Shane Yu <shanesimpkins@gmail.com>
2020-04-21 21:28:23 +0100

    ✨ add initial workspace/mod project

---
+++ src/main/java/com/github/shaneyu/tutorialmod/TutorialMod.java
@@ -0,0 +1,9 @@
+package com.github.shaneyu.tutorialmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("tutorialmod")
+public class TutorialMod
+{
+}
